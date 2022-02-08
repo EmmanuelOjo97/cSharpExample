92c9487981f5eeac73fae9d1a4b14d3422ce5b87
EmmanuelOjo97 <eojo1997@gmail.com>
2022-02-08 13:16:21 +0000

    switch statements added

--- Program.cs
+++ Program.cs
@@ -18,10 +18,54 @@ namespace PainterList{
             int labourCostPerhour = 30;
             double totalCost = labourCostPerhour * hoursTakenToComplete + costOfPaint;
             
+
             Console.WriteLine(coloursToBuy);
             Console.WriteLine(numberOfTins);
             Console.WriteLine(hoursTakenToComplete);
-            Console.WriteLine(totalCost);
+
+            string paintColour = "green";
+            switch(paintColour){
+                case "yellow":
+                Console.WriteLine("Buy yellow paint");
+                break;
+                case "blue":
+                Console.WriteLine("Buy blue paint");
+                break;
+                case "red":
+                Console.WriteLine("Buy red paint");
+                break;
+                case "purple":
+                Console.WriteLine("Buy purple paint");
+                break;
+                default:
+                Console.WriteLine("Paint out of Stock");
+                break;
+            }
+
+            if(totalCost > 150){
+                Console.WriteLine("Total cost before discount £" + totalCost);
+                totalCost*=0.9;
+                Console.WriteLine("10% discount applied: £" + totalCost);
+                return;
+            }else{
+            Console.WriteLine("Total cost with no discount " + totalCost);
+            return;
+            }
+
+            // int x = 5;
+            // int y = 10;
+
+            // if(x == y){
+            //     Console.WriteLine(1);
+            //     return;
+            // }else if(x > y){
+            //     Console.WriteLine(2);
+            // }else{
+            //     Console.WriteLine(3);
+            // }
+
+
+
 
 
 
