7f03889ef31144ef8cf5456968a86e78c4d37409
EmmanuelOjo97 <eojo1997@gmail.com>
2022-02-08 11:02:46 +0000

    varible grouping

--- Program.cs
+++ Program.cs
@@ -5,15 +5,19 @@
 namespace PainterList{
     class Program{
         static void Main (string[]args){
+            // tins of paint
             double colourRed = 10.99;
             double colourBlue = 9.99;
             double colourPurple = 13.09;
+            // paint colours required
             string coloursToBuy = "redx2,blue,purple";
+            //cost of paint and labour
             double costOfPaint = colourRed * 2 + colourBlue + colourPurple;
             int numberOfTins = 4;
             double hoursTakenToComplete = 5.5;
             int labourCostPerhour = 30;
             double totalCost = labourCostPerhour * hoursTakenToComplete + costOfPaint;
+            
             Console.WriteLine(coloursToBuy);
             Console.WriteLine(numberOfTins);
             Console.WriteLine(hoursTakenToComplete);
