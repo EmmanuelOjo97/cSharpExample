52b5b2b7bfd5f9ce9000773e1285646e43a9f872
EmmanuelOjo97 <eojo1997@gmail.com>
2022-02-08 09:37:31 +0000

    this is my first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+namespace PainterList{
+    class Program{
+        static void Main (string[]args){
+            double colourRed = 10.99;
+            double colourBlue = 9.99;
+            double colourPurple = 13.09;
+            string coloursToBuy = "redx2,blue,purple";
+            int numberOfTins = 4;
+            double hoursTakenToComplete = 5.5;
+            Console.WriteLine(coloursToBuy);
+            Console.WriteLine(colourRed*2 + colourBlue + colourPurple );
+            Console.WriteLine(numberOfTins);
+            Console.WriteLine(hoursTakenToComplete);
+            double nice = Math.Abs(-30.9);
+            Console.WriteLine(nice);
+        }
+    }
+}
